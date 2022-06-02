dee3d45b9d6fd48bdcd97c82e676e0aa1d86f9f9
Nickzbt <104036146+Nickzbt@users.noreply.github.com>
2022-06-02 18:22:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+    Assalto ao banco ocorrido durante a noite entre 8 PM e 5 AM, onde o alarme não foi 
+    tocado e câmeras de segurança foram desligadas, descubra através das pistas quem 
+    é o culpado pelo crime, baseando-se nos álibis dados pelos suspeitos do crime.
+    
+    # --------
+    
+    Suspeitos:
+        - Guarda 1;
+        - Guarda 2;
+        - Zelador;
+        - Gerente do banco;
+        - Atendente 1;
+        - Atendente 2;
+        - Atendente 3;
+    Culpado:
+        - Atendente 1;
+    
+    # --------
+
+    Hipótese/Álibis:
+        - G1: Fechou o banco as 8 PM.
+        - G2: Enquanto G1 fechava o banco, G2 verificava os ambientes internos.
+        - Z : Chegou ao fechamento 8 PM para começar a limpeza, afirma não ter visto nada
+                de estranho e nenhum desconhecido no banco.
+        - GB: Documentou valores do cofre até as 9 PM.
+        - GB: Trancou o cofre as 9 PM.
+        - A1: Saiu mais cedo às 7:15 PM devido a um compromisso.
+        * A1: Foi a uma consulta médica as 7:30 PM.
+        - A2: Saiu as 8:20 PM, junto do A3.
+        - A3: Saiu as 8:20 PM, junto do A2.
+        - G1: Foi conferir o cofre as 10:15 PM.
+        - Z : Foi limpar a região do cofre as 10:20 PM.
+        - P1: Os dois afirmam não ter encontrado ninguém.
+        - P2: A1 não apresentou atestado no outro dia.
+        - P3: A2 e A3 chegaram 1h atrasados no dia seguinte.
+        - A2: Não se lembra daquela noite após o trabalho.
+        - A3: Informou que ele/a e A2 foram em uma festa de um conhecido e beberam.
+        - P4: Caiu a luz da região das 12 PM as 2 AM.
+        - G1: Foi conferir o quadro de luz depois da queda.
+        - G1: Ligou o gerador reserva as 12:30 PM, e as câmeras voltaram 30 min depois.
+        - G2: Depois da queda de luz foi fazer uma ronda no banco.
+        - Z : Foi para casa após a volta da energia às 2 AM.
+"""
